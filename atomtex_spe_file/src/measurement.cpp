76174778f0f04efd6814651eafb59c795279c299
o.pyhov <pyhovoleg31@gmail.com>
2025-07-07 21:36:24 +0500

    Check NaN

--- atomtex_spe_file/src/measurement.cpp
+++ atomtex_spe_file/src/measurement.cpp
@@ -1,5 +1,6 @@
 #include <stdexcept>
 #include <format>
+#include <cmath>
 
 #include "atomtex_spe_file/measurement.hpp"
 
@@ -8,7 +9,7 @@ namespace atomtex_spe_file
 
 Longitude::Longitude(const double dd) : dd_(dd)
 {
-    if (dd_ < -180 || dd_ > 180)
+    if (dd_ < -180 || dd_ > 180 || std::isnan(dd_))
     {
         const auto error{
             std::format("Invalid decimal degrees for longitude: {}", dd_)};
@@ -23,7 +24,7 @@ double Longitude::DecimalDegrees() const noexcept
 
 Latitude::Latitude(const double dd) : dd_(dd)
 {
-    if (dd_ < -90 || dd_ > 90)
+    if (dd_ < -90 || dd_ > 90 || std::isnan(dd_))
     {
         const auto error{
             std::format("Invalid decimal degrees for latitude: {}", dd_)};
@@ -39,7 +40,7 @@ double Latitude::DecimalDegrees() const noexcept
 DoseRate::DoseRate(const double microSvPerHour)
     : microSvPerHour_(microSvPerHour)
 {
-    if (microSvPerHour_ < 0)
+    if (microSvPerHour_ < 0 || std::isnan(microSvPerHour))
     {
         const auto error{std::format("Invalid dose rate: {}", microSvPerHour_)};
         throw std::range_error(error);

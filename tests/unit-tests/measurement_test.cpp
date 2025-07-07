c6dbac3fc5be788048ea3d143d40a90e6fbe3f41
o.pyhov <pyhovoleg31@gmail.com>
2025-07-07 21:35:04 +0500

    Longitude test

--- tests/unit-tests/measurement_test.cpp
+++ tests/unit-tests/measurement_test.cpp
@@ -1,6 +1,29 @@
+#include <exception>
+#include <cmath>
+
 #include <gtest/gtest.h>
 
-TEST(Test, Test)
+#include "atomtex_spe_file/measurement.hpp"
+
+using namespace atomtex_spe_file;
+
+TEST(LongitudeTest, ConstructorWorks)
 {
-    ASSERT_TRUE(false);
+    Longitude lon{10};
+    EXPECT_FLOAT_EQ(lon.DecimalDegrees(), double{10});
+}
+
+TEST(LongitudeTest, TooLargeDD)
+{
+    EXPECT_THROW(Longitude{180.0001}, std::exception);
+}
+
+TEST(LongitudeTest, TooSmallDD)
+{
+    EXPECT_THROW(Longitude{-180.0001}, std::exception);
+}
+
+TEST(LongitudeTest, NaN)
+{
+    EXPECT_THROW(Longitude{NAN}, std::exception);
 }
